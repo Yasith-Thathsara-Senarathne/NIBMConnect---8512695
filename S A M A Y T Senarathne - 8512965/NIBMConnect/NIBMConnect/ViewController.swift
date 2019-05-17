2654b079b488f496fee3470b1f2bb04b600b9fd2
Yasith Thathsara Senarathne <47640360+Yasith-Thathsara-Senarathne@users.noreply.github.com>
2019-05-17 06:46:36 +0530

    Add files via upload

---
+++ S A M A Y T Senarathne - 8512965/NIBMConnect/NIBMConnect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NIBMConnect
+//
+//  Created by Yasith Thathsara Senarathne on 5/17/19.
+//  Copyright © 2019 Yasith Thathsara Senarathne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
